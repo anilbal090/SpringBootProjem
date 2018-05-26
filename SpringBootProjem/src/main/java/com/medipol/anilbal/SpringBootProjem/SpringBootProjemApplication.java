f97e841a75779400aa8d9573917206eaaf16a38c
crjje <crjje@DESKTOP-84Q05TT>
2018-05-27 02:31:04 +0300

    Githuba commitleme

---
+++ SpringBootProjem/src/main/java/com/medipol/anilbal/SpringBootProjem/SpringBootProjemApplication.java
@@ -0,0 +1,12 @@
+package com.medipol.anilbal.SpringBootProjem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjemApplication.class, args);
+	}
+}
